5830eb1f4ec394a38dca86ac7793b22132f9b4a2
yji35j42012 <yji35j42012@gmail.com>
2020-12-07 15:54:07 +0800

    第五週調整

---
+++ js/week7.js
@@ -0,0 +1,12 @@
+let data = {
+    fined:true,
+    verified_box: false,
+    verified_state: false,
+    cu_box: false,
+    cu_state: '',
+    cu_btn_d: false,
+
+    fined:false,
+    page_box: false,
+    reply_box: false,
+}
